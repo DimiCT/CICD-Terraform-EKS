0d2adf5620803b47b2af42dc9f9a7df962526316
DimiCT <EM104862.EXTERNAL@ctingenieros.es>
2024-07-31 10:57:53 +0300

    Update main.tf
    change cluster name

--- EKS/main.tf
+++ EKS/main.tf
@@ -15,15 +15,15 @@ module "vpc" {
   single_nat_gateway   = true
 
   tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/cluster/awake-cluster" = "shared"
   }
   public_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/cluster/awake-cluster" = "shared"
     "kubernetes.io/role/elb"               = 1
 
   }
   private_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/cluster/awake-cluster" = "shared"
     "kubernetes.io/role/private_elb"       = 1
 
   }
@@ -33,7 +33,7 @@ module "vpc" {
 
 module "eks" {
   source                         = "terraform-aws-modules/eks/aws"
-  cluster_name                   = "my-eks-cluster"
+  cluster_name                   = "awake-cluster"
   cluster_version                = "1.29"
   cluster_endpoint_public_access = true
   vpc_id                         = module.vpc.vpc_id
