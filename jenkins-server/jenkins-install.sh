09eb5ca9956d27868c23fa37fcc3316968b9ac90
DimiCT <dimitri.griparis@ctengineeringgroup.com>
2024-08-05 13:15:57 +0300

    Update jenkins-install.sh
    changed jenkins install script to test working copy from jenkins website

--- jenkins-server/jenkins-install.sh
+++ jenkins-server/jenkins-install.sh
@@ -4,14 +4,31 @@
 sudo apt update
 sudo apt install openjdk-11-jre -y
 ===========================================================================================
-# Jenkins installation 
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
+#  Jenkins installation 
+# curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+#   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+# echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+#   https://pkg.jenkins.io/debian binary/ | sudo tee \
+#   /etc/apt/sources.list.d/jenkins.list > /dev/null
+# sudo apt-get update
+# sudo apt-get install jenkins -y
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins -y
+sudo apt-get install jenkins
+
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre
+java -version
+openjdk version "17.0.8" 2023-07-18
+OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
+OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
 
 ===========================================================================================
 # Terraform Installation
