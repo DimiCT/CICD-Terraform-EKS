5a3caeede38f6214a50667441a585df6c9b90d65
DimiCT <dimitri.griparis@ctengineeringgroup.com>
2024-08-05 08:46:09 +0300

    Update jenkins-install.sh
    add helm installation to script

--- jenkins-server/jenkins-install.sh
+++ jenkins-server/jenkins-install.sh
@@ -3,7 +3,7 @@
 
 sudo apt update
 sudo apt install openjdk-11-jre -y
-
+===========================================================================================
 # Jenkins installation 
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
@@ -13,6 +13,7 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 sudo apt-get update
 sudo apt-get install jenkins -y
 
+===========================================================================================
 # Terraform Installation
 
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
@@ -27,7 +28,29 @@ https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 sudo apt-get install terraform -y
+#!/bin/bash
 
+===========================================================================================
+#Install Helm 
+# Update package list
+sudo apt-get update
+
+# Install necessary prerequisites
+sudo apt-get install -y apt-transport-https curl
+
+# Download the Helm installation script
+curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 > get_helm.sh
+
+# Make the script executable
+chmod +x get_helm.sh
+
+# Run the Helm installation script
+./get_helm.sh
+
+# Verify the installation
+helm version
+
+============================================================================================
 # Installing kubernetes
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
